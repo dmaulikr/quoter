d0142b3314c6701aeabefe7c0b664fb22546ef77
nikitavoloboev <nikita.voloboev@gmail.com>
2016-08-05 22:55:56 +0200

    init

---
+++ quoter/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  quoter
+//
+//  Created by nikivi on 02/07/2016.
+//  Copyright © 2016 nikitavoloboev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+    
+    @IBOutlet weak var transferText: NSButton!
+    
+    @IBOutlet weak var quoteInput: NSTextField!
+
+    
+    
+    @IBOutlet weak var transferredText: NSTextField!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification)
+    
+    {
+        
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+
+        
+        
+    }
+
+
+}
+
